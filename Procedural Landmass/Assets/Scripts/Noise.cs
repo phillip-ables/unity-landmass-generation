6e5f7792610a6d636aad04107380086b4ae3a0a6
Phillip <phillipables1@gmail.com>
2018-03-21 20:42:29 -0400

    noise script with MonoBehavior removed and GenerateNoiseMap that returns a float array and takes a width and height type int for a map

---
+++ Procedural Landmass/Assets/Scripts/Noise.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {  // were not attaching this to any object, no need MonoBehaviour
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight)
+    {
+
+    }
+}
