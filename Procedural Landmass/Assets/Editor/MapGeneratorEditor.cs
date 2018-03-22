45c0a63d7e6b5b36e69fd7ec5f45e401df268b6a
Phillip <phillipables1@gmail.com>
2018-03-22 08:17:50 -0400

    tell the editor script what type of customEditor it is for it to work (MapGenerator)

--- Procedural Landmass/Assets/Editor/MapGeneratorEditor.cs
+++ Procedural Landmass/Assets/Editor/MapGeneratorEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[CustomEditor (typeof (MapGenerator))]
 public class MapGeneratorEditor : Editor {
 
     public override void OnInspectorGUI()
