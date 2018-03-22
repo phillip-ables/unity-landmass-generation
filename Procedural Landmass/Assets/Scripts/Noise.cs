a5b1a15ab1e1e6e8b0a8a53b35bfaacd83d19e5e
Phillip <phillipables1@gmail.com>
2018-03-21 23:20:07 -0400

    for each iteration set sample floats for x and y to x and y create a scale variable to devide samples by to give us a float value, create float argument for scale input, if scale is 0 then bad so 0 now equals .0000001f

--- Procedural Landmass/Assets/Scripts/Noise.cs
+++ Procedural Landmass/Assets/Scripts/Noise.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Noise {  // were not attaching this to any object, no need MonoBehaviour
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        if (scale <= 0)
+            scale = 0.0001f;
 
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                
+                float sampleX = x / scale;
+                float sampleY = y / scale;
             }
         }
     }
