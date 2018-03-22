8a84e51b32bc64f2d9c20b0a07244e65fba0c412
Phillip <phillipables1@gmail.com>
2018-03-21 23:23:17 -0400

    now that we have our variables -> perlinValue of mathf perlinoise with our samples passed through we then pass those values into our noiseMap and after iteration return noiseMAp

--- Procedural Landmass/Assets/Scripts/Noise.cs
+++ Procedural Landmass/Assets/Scripts/Noise.cs
@@ -16,7 +16,11 @@ public static class Noise {  // were not attaching this to any object, no need M
             {
                 float sampleX = x / scale;
                 float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
             }
         }
+        return noiseMap;
     }
 }
