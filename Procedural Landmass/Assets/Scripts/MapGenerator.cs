c3e46a64fea5d6cc388921983ece55171aa892b0
Phillip <phillipables1@gmail.com>
2018-03-21 23:35:38 -0400

    MapDisplay script added to MapGenerator game object

--- Procedural Landmass/Assets/Scripts/MapGenerator.cs
+++ Procedural Landmass/Assets/Scripts/MapGenerator.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        
+    }
 }
