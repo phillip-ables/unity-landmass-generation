c45ea3a3ae7ad00bd5432ee640605130ddcfb5d9
Phillip <phillipables1@gmail.com>
2018-03-28 00:01:02 -0400

    remove our noise map method from map display and put it in a new method in texture generator to make height map there and pass it to our other method in texture generator

--- Procedural Landmass/Assets/Scripts/TextureGenerator.cs
+++ Procedural Landmass/Assets/Scripts/TextureGenerator.cs
@@ -10,4 +10,22 @@ public static class TextureGenerator
         texture.Apply();
         return texture;
     }
+
+    public static Texture2D TextureFromHeightMap(float[,] heightMap)
+    {
+
+
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+        return TextureFromColorMap(colorMap, width, height);
+    }
 }
