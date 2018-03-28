debd21e73b02ce8e439bd80687076d15a8d6951b
Phillip <phillipables1@gmail.com>
2018-03-28 08:46:00 -0400

    declare meshData and instantaite vertexIndex of 0, then in GenerateTerrainMesh main coordinate iteration we want meshData Vertices at index vertex index to equal new vector3 of x position height map at x and y and finally y position

--- Procedural Landmass/Assets/Scripts/MeshGenerator.cs
+++ Procedural Landmass/Assets/Scripts/MeshGenerator.cs
@@ -23,9 +23,19 @@ public class MeshData
     public Vector3[] vertices;
     public int[] triangles;
 
+    int triangleIndex;
+
     public MeshData(int meshWidth, int meshHeight)
     {
         vertices = new Vector3[meshWidth * meshHeight];
         triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex+1] = b;
+        triangles[triangleIndex+2] = c;
+        triangleIndex += 3;
     }
 }
